c9da632e1ad5b8ca634f7d00c51274e2ae8e7d18
Maria <thenoune@users.noreply.github.com>
2016-07-05 14:30:55 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+﻿angular.module('sortApp', [])
+
+.controller('mainController', function($scope) {
+    $scope.sortType = 'name'; // set the default sort type
+    $scope.sortReverse = 'false'; // set the default sort order
+    $scope.searchLocations = ''; //set the default search/filter term
+
+    //create list of locations
+    $scope.places = [
+        {name: 'Kansas City', state: 'MO', hotelCost: '0'},
+        {name: 'Minneapolis', state:'MI', hotelCost: '150'},
+        {name: 'San Franscisco', state:'CA', hotelCost: '199'},
+        {name: 'Myrtle Beach', state:'NC', hotelCost: '199'}
+];
+});
